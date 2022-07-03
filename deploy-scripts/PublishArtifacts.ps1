f9df6ca3e93dfb9cf425b9581215b3743561bdf7
AliYousifThe2nd <alooy3d@gmail.com>
2022-07-03 11:48:31 +0300

    Added deploy-scripts

---
+++ deploy-scripts/PublishArtifacts.ps1
@@ -0,0 +1,14 @@
+$target = "C:\Users\Administrator\Desktop\out" 
+
+function DeleteIfExistsAndCreateEmptyFolder($dir )
+{
+    if ( Test-Path $dir ) {    
+           Get-ChildItem -Path  $dir -Force -Recurse | Remove-Item -force –recurse
+           Remove-Item $dir -Force
+    }
+    New-Item -ItemType Directory -Force -Path $dir
+}
+# Clean up target directory
+DeleteIfExistsAndCreateEmptyFolder($target )
+
+dotnet publish TestAPI -o C:\Users\Administrator\Desktop\out
